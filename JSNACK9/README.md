9fff2ff4de02f74c29179fb7bff128969e667358
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-05-06 00:05:44 +0200

    [UPD] JSNACK9 done

---
+++ JSNACK9/README.md
@@ -0,0 +1,13 @@
+Snack 9
+
+    Calcola la somma e la media dei primi 10 numeri.
+    Stampa i risultati in console
+
+
+
+    // - definisco una variabile che parta da valore = 0 (somma)
+    // - creo un ciclo for che si ripeta per 10 volte dove inserisco l'operazione
+    // - calcolo la somma dei numeri da 1 a 10
+
+    // - fuori dal ciclo calcolo la media dei numeri
+    // - stampo i risultati somma e media in console
--- JSNACK9/main.js
+++ JSNACK9/main.js
@@ -1,2 +1,26 @@
 'use strict';
 console.log('ok');
+
+
+
+    // - definisco una variabile che parta da valore = 0 (somma)
+    let somma = 0;
+    const count = 10;
+    // - creo un ciclo for che si ripeta per 10 volte dove inserisco l'operazione
+    for (let i = 1; i <= count; i++) {
+          // - calcolo la somma dei numeri da 1 a 10
+        somma += i;
+      
+    }
+  
+  
+
+    // - fuori dal ciclo calcolo la media dei numeri
+
+    let media = somma / 10;
+
+
+    // - stampo i risultati somma e media in console
+
+    console.log('la somma dei primi dieci numeri è:', somma);
+    console.log('la media dei primi dieci numeri è:', media);
