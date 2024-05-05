62a448bfe7243dc2d30b8747277fa5aa6674b9d9
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-05-05 23:24:47 +0200

    [UPD] JSNACK 5 done

---
+++ JSNACK5/README.md
@@ -0,0 +1,13 @@
+
+Snack 5
+    // - creo un array vuoto
+    Crea un array vuoto.
+    // - creo un ciclo for che si ripeta per sei volte
+    // definisco una variabile che chieda all'utente di inserire un numero intero e lo salvo in un prompt (userNumber)
+    Chiedi per 6 volte all’utente di inserire un numero,
+
+   SE numero utente disparo ALLORA inserisco nell'array 
+    Se è dispari inseriscilo nell’array.
+    // - stampo in console il risultato 
+    Stampa in console l'array risultante.
+
---
+++ JSNACK5/main.js
@@ -0,0 +1,25 @@
+'use strict';
+console.log('ok');
+
+// Snack 5
+//     // - creo un array vuoto
+let emptyArray = [];
+
+
+//     // - creo un ciclo for che si ripeta per sei volte
+const count = 6;
+for (let i = 0; i < count; i++) {
+//     // definisco una variabile che chieda all'utente di inserire un numero intero e lo salvo in un prompt (userNumber)
+    const userNumber = Number(prompt(`${i} di ${count} inserisci un numero`));
+    console.log('il numero inserito è:', userNumber);
+    //    SE numero utente disparo ALLORA inserisco nell'array 
+    if (userNumber % 2 !== 0) {
+        emptyArray.push(userNumber);
+
+    }  
+}
+  //     // - stampo in console il risultato 
+  console.log('i numeri dispari sono:', emptyArray);
+
+
+
